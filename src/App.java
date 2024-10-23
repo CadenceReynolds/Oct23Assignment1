e37d1c29ecd6c42f50c4ed58a0b7a6ea966fb453
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-10-23 13:36:27 -0400

    Finished assignment

--- src/App.java
+++ src/App.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class App {
+public class App {
+    public static void main(String[] args) {
+
+        Library A = new Library();
+        A.name = "A";
+        A.addBooks(100);
+
+        Library B = new Library();
+        B.name = "B";
+        B.addBooks(100);
+        System.out.println("Total books in all libraries: " + Library.getTotalBooks());
+    }
 }
